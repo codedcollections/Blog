96e01518d6a2b40bd27ba67c92d84b7a1b2e107b
codedcollections <ma.unander@gmail.com>
2025-10-01 15:55:18 +0200

    add appendChild grid and flex

--- script.js
+++ script.js
@@ -15,9 +15,20 @@ Bygg en bloggplattform som innehåller följande delar:
 4. **Responsiv Design:**
     - Se till att webbplatsen fungerar bra på olika enheter, både på dator och mobil. Använd CSS för att säkerställa en bra användarupplevelse på alla skärmstorlekar.
 */
+const postArray = [
+    {
+        title: "title of post",
+        text: "aksa dladksak da lkds dlsakalkdalkdslkdaalksdlkas dlka dlka dsald ksal dlksadlkadlksalkdak d"
+    },
+    {
+        title: "second post",
+        text: "aksld masdlm saakl dmkdlamakslm dlksa dmsa dsam ldam dklsam dakldmsldamdkd almldsk"
+    }
+]
 const currentName = document.querySelector("#name")
 const textInput = document.querySelector("#text-input")
 const submitBtn = document.querySelector("#post")
+const latestDiv = document.querySelector(".latest")
 
 function addPost(e){
     //Stop from page reload on submitbtn click
@@ -28,3 +39,16 @@ function addPost(e){
 
 }
 submitBtn.addEventListener('click',addPost)
+
+function showPosts(post){
+    const newDiv = document.createElement("div")
+    newDiv.className = "post"
+    const newTitle = document.createElement("h2")
+    newTitle.textContent = post.title
+    newDiv.appendChild(newTitle)
+    const newText = document.createElement("p")
+    newText.textContent = post.text
+    newDiv.appendChild(newText)
+    latestDiv.appendChild(newDiv)
+}
+postArray.forEach(showPosts)
