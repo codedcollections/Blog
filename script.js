2a9b0f6ca549320b6ec7d0dedfe8b15ac3019e0b
codedcollections <ma.unander@gmail.com>
2025-10-13 15:12:53 +0200

    Add functionality to add post using array and creating HTML structure for post

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ let postArray =[
         "date":dateAndTime
     }
 ]
-
+const AllPostsContainer = document.querySelector(".all-posts")
 function renderPost(postObject){
     //Holds all information about a post
     const postContainer = document.createElement("div")
@@ -30,34 +30,52 @@ function renderPost(postObject){
     //div for delete button
     const deleteContainer = document.createElement("div")
     deleteContainer.className = "delete-div"
-    const deleteBtn = document.createElement("button")
-    deleteBtn.className = "delete-btn"
+    const deleteBtn = document.createElement("img")
     deleteBtn.src = "assets/symbols/trash.svg"
+    deleteBtn.alt = "trash can symbol"
+    deleteBtn.className = "delete-btn"
     deleteBtn.addEventListener('click',()=>{postContainer.remove()})
+    deleteContainer.appendChild(deleteBtn)
+    postContainer.appendChild(deleteContainer)
     //information presented in post
     const postTitle = document.createElement("h2")
     postTitle.className="post-title"
+    postTitle.textContent = postObject.title
+    postContainer.appendChild(postTitle)
+
     const postInfo = document.createElement("p")
     //contains date and name of post author
     postInfo.className = "post-info"
+    const postedBy = `Posted by ${postObject.author} ${postObject.date}`
+    postInfo.textContent = postedBy
+    postContainer.appendChild(postInfo)
+
     const postText = document.createElement("p")
     postText.className="post-text"
+    postText.textContent = postObject.postContent
+    postContainer.appendChild(postText)
+
     const likesContainer = document.createElement("div")
     likesContainer.className = "likes-div"
+
     const likeBtn = document.createElement("button")
     likeBtn.textContent = 0
-    likeBtn.addEventListener('click',()=>{newLikeBtn.textContent++})
+    likeBtn.addEventListener('click',()=>{likeBtn.textContent++})
+    likesContainer.appendChild(likeBtn)
     const dislikeBtn = document.createElement("button")
     dislikeBtn.textContent = 0
-    dislikeBtn.addEventListener('click',()=>{newDislikeBtn.textContent--})
-
-
+    dislikeBtn.addEventListener('click',()=>{dislikeBtn.textContent--})
+    likesContainer.appendChild(dislikeBtn)
+    postContainer.appendChild(likesContainer)
+    AllPostsContainer.appendChild(postContainer)
 }
 
-postArray.forEach((element) => console.log(element))
-console.log("end of forEach")
-const postContainer = document.querySelector(".posts")
+/* postArray.forEach((element) => console.log(element))
+console.log("end of forEach") */
 
+postArray.forEach((element) => renderPost(element))
+
+/* const postContainer = document.querySelector(".posts")
 const newDiv = document.createElement("div")
 newDiv.style.backgroundColor="red"
 const newDeleteBtn = document.createElement("button")
@@ -77,7 +95,7 @@ newDislikeBtn.textContent = 0
 newDislikeBtn.addEventListener('click',()=>{newDislikeBtn.textContent--})
 newDiv.appendChild(newDislikeBtn)
     
-postContainer.appendChild(newDiv)
+postContainer.appendChild(newDiv) */
 
 
 function checkInput(e){
